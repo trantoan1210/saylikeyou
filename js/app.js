61a92a02d7c989ecbfa0580dfab35e14558dabc3
Toàn Trần <neo290992@gmail.com>
2021-06-25 22:43:23 +0700

    tao

---
+++ js/app.js
@@ -0,0 +1,4 @@
+function setRandomPosition(){
+        document.getElementById("btn-no").style.top = Math.floor(Math.random() * 200) + "px";
+        document.getElementById("btn-no").style.left = Math.floor(Math.random() * 1000) + "px";
+        }
